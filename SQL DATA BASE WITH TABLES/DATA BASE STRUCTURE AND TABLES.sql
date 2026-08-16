6759f6bf13046c5e29e03eb02425cda0e499f173
pavan kumar <jpavanreddy21@gmail.com>
2026-08-16 17:16:59 +0530

    Add files via upload

---
+++ SQL DATA BASE WITH TABLES/DATA BASE STRUCTURE AND TABLES.sql
@@ -0,0 +1,10 @@
+CREATE database ecommerce_analytics
+
+USE ecommerce_analytics;
+SELECT *FROM customers;
+SELECT*FROM categories;
+SELECT *FROM products;
+SELECT*FROM orders;
+SELECT*FROM order_items;
+SELECT*FROM payments;
+SELECT*FROM returns;
